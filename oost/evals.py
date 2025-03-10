6552fefe4456bff9ef59dc05614033ec2c5a4053
RealPolitiX <xrpatrick@gmail.com>
2025-03-09 19:08:20 -0700

    Updated evals module

--- oost/evals.py
+++ oost/evals.py
@@ -1,5 +1,9 @@
 import torch
 # from transformers import BertTokenizer, BertModel
+from sentence_transformers import SentenceTransformer, SimilarityFunction
+from evaluate import load
+
+rouge = load("rouge")
 
 def st_score(reference, candidate, model):
 
@@ -8,3 +12,30 @@ def st_score(reference, candidate, model):
     sim = model.similarity(ref_embedding, cand_embedding).item()
     
     return sim
+
+
+class Evaluator:
+
+    def __init__(self, model_string=None, sim_func='cosine', **kwargs):
+
+        if model_string is None:
+            self.model_string = 'sentence-transformers/all-roberta-large-v1'
+        else:
+            self.model_string = model_string
+        # self.model = BertModel.from_pretrained(model_string, torch_dtype=torch.bfloat16)
+
+        if sim_func == 'cosine':
+            self.model = SentenceTransformer(self.model_string, similarity_fn_name=SimilarityFunction.COSINE, **kwargs)
+        elif sim_func == 'dotprod':
+            self.model = SentenceTransformer(self.model_string, similarity_fn_name=SimilarityFunction.DOT_PRODUCT, **kwargs)
+        elif sim_func == 'euclidean':
+            self.model = SentenceTransformer(self.model_string, similarity_fn_name=SimilarityFunction.EUCLIDEAN, **kwargs)
+
+    def compare(self, ref, gt):
+
+        self.st_score = st_score(ref, gt, self.model)
+
+    def rouge(self, ref, gt):
+
+        self.rouge_score = rouge.compute(predictions=[ref], references=[gt])
+        self.rougeL = self.rouge_score['rougeL']
